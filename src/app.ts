25b027f5d715487ae0256254db9bb5b5d5183187
Kj-RahiL <rahiilarham@gmail.com>
2024-11-30 21:52:59 +0600

    server is running on the localport

---
+++ src/app.ts
@@ -0,0 +1,51 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+
+
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+// app.use('/api', router);
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+      <html>
+        <head>
+          <title>Recipe Sharing Community Server</title>
+          <style>
+            body {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              height: 100vh;
+              margin: 0;
+            }
+            .content {
+              text-align: justify;
+              font-size: 36px;
+              width: 80%; 
+            }
+          </style>
+        </head>
+        <body>
+          <div class="content">
+            Boom Boom 💥💥!! Gym Class Schedule and Management Server App is running...
+          </div>
+        </body>
+      </html>
+    `);
+});
+
+app.use(globalErrorHandler);
+
+// not found route error
+app.use(notFound);
+
+export default app;
